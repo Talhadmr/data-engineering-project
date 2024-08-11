25a6a58a73cacec8572aa83b9dfe5323bef89ba2
Talhadmr <demirtalha093@gmail.com>
2024-08-11 22:21:44 +0300

    db connected

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import os
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+
 from src.convertor import Convertor
+from src.db_connection import PostgreSQLDB
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)    
 
@@ -8,7 +13,12 @@ def main():
     convertor = Convertor('data/yellow_tripdata_2022-01.parquet')
     convertor.convert_to_csv()
     df = pd.read_csv('data/yellow_tripdata_2022-01.csv', nrows=100)
-    print(df.head())
+
+    db = PostgreSQLDB()
+    db.connect()
+    
+
+
 
 if __name__ == "__main__":
     main()
