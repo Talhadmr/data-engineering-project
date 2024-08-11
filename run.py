b23b28408aa950361121b667b2e5021d8f74e42a
Talhadmr <demirtalha093@gmail.com>
2024-08-11 23:58:09 +0300

    data added to db

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 from src.convertor import Convertor
 from src.db_connection import PostgreSQLDB
+from src.data_io_manager import PostgresDataHandler
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)    
@@ -12,11 +13,12 @@ pd.set_option('display.max_rows', None)
 def main():
     convertor = Convertor('data/yellow_tripdata_2022-01.parquet')
     convertor.convert_to_csv()
+
     df = pd.read_csv('data/yellow_tripdata_2022-01.csv', nrows=100)
 
-    db = PostgreSQLDB()
-    db.connect()
-    
+    postgres_data_handler = PostgresDataHandler()
+    postgres_data_handler.write(df, 'yellow_tripdata_2022_01')    
+
 
 
 
